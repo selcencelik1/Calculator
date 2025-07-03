9c610556eaed46ebf53b5d2009d38be7f1ee983e
Selcen Çelik <sselcencelikk@gmail.com>
2025-07-03 13:58:39 +0300

    Calculator class done

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public double add(double a,double b){
+        return a+b;
+    }
+    public double substract(double a,double b){
+        return a-b;
+    }
+    public double multiply(double a,double b){
+        return a*b;
+    }
+    public double divide(double a,double b){
+        if(b == 0) {
+            System.out.println("Zero division error!");
+            return Double.NaN;
+        }
+        return a/b;
+    }
+}
