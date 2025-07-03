ec30dd6a1d85b183f126f4a0bc8cceac558d02d7
Selcen Çelik <sselcencelikk@gmail.com>
2025-07-03 14:03:29 +0300

    main class done

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public double add(double a,double b){
         return a+b;
     }
-    public double substract(double a,double b){
+    public double subtract(double a,double b){
         return a-b;
     }
     public double multiply(double a,double b){
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("\n---Calculator---");
+            System.out.println("[1] Addition");
+            System.out.println("[2] Subtraction");
+            System.out.println("[3] Multiplication");
+            System.out.println("[4] Divison");
+            System.out.println("[5] Exit");
+            int secim = scanner.nextInt();
+            if (secim == 0) {
+                System.out.println("Çıkılıyor...");
+                break;
+            }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.print("1. sayı: ");
+            double sayi1 = scanner.nextDouble();
+            System.out.print("2. sayı: ");
+            double sayi2 = scanner.nextDouble();
+
+            double sonuc = 0;
+
+            switch (secim) {
+                case 1:
+                    sonuc = calc.add(sayi1, sayi2);
+                    break;
+                case 2:
+                    sonuc = calc.subtract(sayi1, sayi2);
+                    break;
+                case 3:
+                    sonuc = calc.multiply(sayi1, sayi2);
+                    break;
+                case 4:
+                    sonuc = calc.divide(sayi1, sayi2);
+                    break;
+                default:
+                    System.out.println("Geçersiz seçim!");
+                    continue;
+            }
+
+            System.out.println("Sonuç: " + sonuc);
         }
+
+        scanner.close();
     }
 }
